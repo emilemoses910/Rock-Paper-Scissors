819148aa4768a66aed1134ed8dbd0ea4dcba03d7
Emile Mo <emile.moses@gmail.com>
2023-06-08 00:45:26 +0200

    Write a function that plays a single round
    Create function takes two words as parameters to indicate each player's selection. It compares the words to
    determine winner.
    *Function called playRound with parameters playerSelection & computerSelection.
    *Use 'if' method to to determine/return a draw and 'else if' method to indicate a win and else if function to
    determine a loose.
    *For both win and loose, there are a paper, beats rock, rock beats scissors and scissors beats paper
    for scenario for each player. Use manual, constant input for player 1. and use the function
    getComputerChoice for player 2.
    *Then console.log the playround function.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,12 +1,40 @@
-console.log("Hello Emile")
 
 function getComputerChoice(){
-    const words =['Rock', 'Paper', 'Scissor'];
+    const words =['Rock', 'Paper', 'Scissors'];
     let randomIndex = Math.floor(Math.random() * words.length);
+         
     return words[randomIndex];
     }
     
     let randomWord = getComputerChoice();
-    console.log(randomWord);
+    
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Draw!';}
+        else if (playerSelection ==='Rock' && computerSelection ==='Scissors') {
+            return 'You Win!';}
+            else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+            return 'You Win!';}
+            else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+            return 'You Win!';}
+        else if (playerSelection==='Scissors' && computerSelection==='Rock') {
+            return 'You Loose!';}
+            else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+            return 'You Loose!';}
+            else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+            return 'You Loose!';}
+}
+    const playerSelection = 'Paper';
+    const computerSelection = getComputerChoice();
+
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerSelection)
+    console.log(computerSelection);
+
+    let game () = {playRound(); playRound(); playRound(); playRound()}
+    game()
+
+    //commit and get commit messasge for the above code from CGPT. 
+    
 
